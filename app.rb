97c83d721c83fc5c8bb7b7a1054e6ed0c774e820
otukutun <orehaorz@gmail.com>
2017-01-18 01:53:02 +0900

    apiを作った.

--- app.rb
+++ app.rb
@@ -2,14 +2,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'yaml'
+require 'json'
 require 'erb'
 require 'active_support/all'
 
-get '/' do
+get '/api' do
   @quotes = YAML.load_file("#{settings.root}/quotes.yml")
-  #puts @quotes
-  #@qutes = settings.quotes
-  #puts @quotes.id
+  @quotes.to_json
+end
+
+get '/api/:id' do
+end
+
+get '/' do
   erb :index, layout: :base
 end
 
