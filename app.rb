04305f94059ec6e990cf9721a9001a676e69cd86
otukutun <orehaorz@gmail.com>
2017-01-18 00:48:56 +0900

    名言一覧を見られるようにした.

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 # myapp.rb
 require 'sinatra'
 require 'sinatra/reloader'
+require 'yaml'
 require 'erb'
-#
+
 get '/' do
+  @quotes = YAML.load_file("#{settings.root}/quotes.yml")
+  #puts @quotes
+  #@qutes = settings.quotes
+  #puts @quotes.id
   erb :index, layout: :base
 end
 
